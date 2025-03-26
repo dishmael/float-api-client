b2b69c7c34bb509106485d9e5f2014358359575e
David Ishmael <dishmael3@gmail.com>
2025-03-26 16:03:04 -0400

    removed dotenv load

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,13 @@
-import dotenv from "dotenv";
+/**
+ * @fileoverview Common constants and functions
+ */
 
-dotenv.config(); // load environment variables from .env file
-
+// axios headers
 export const Headers = {
   Authorization: `Bearer ${process.env.API_KEY}`,
   "Content-Type": "application/json",
   "User-Agent": `${process.env.USER_AGENT}`, 
 };
 
+// used for decoding the status code in Float
 export const Status = ["Draft", "Tentative", "Confirmed", "Complete"];
